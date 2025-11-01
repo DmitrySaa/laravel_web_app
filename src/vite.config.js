d7f353c8c35f9bd08fab5a84773d892b64ef1fd3
DmitrySaa <samss4598@gmail.com>
2025-11-01 16:36:29 +0400

    конфиг для Vite

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,13 +1,33 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue'; 
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js', 
+        },
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        watch: {
+            usePolling: true, 
+            interval: 1000,
+            ignored: ['**/node_modules/**', '**/vendor/**', '**/storage/**'],
+        },
+        hmr: {
+            host: 'localhost', // если заходим с локалки
+            protocol: 'ws'
+        }
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        vue(),
         tailwindcss(),
     ],
 });
