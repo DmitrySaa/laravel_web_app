543b29b15183ae97227d2083e1590dfddcb46c15
DmitrySaa <samss4598@gmail.com>
2025-11-01 16:36:58 +0400

    подключение Vue.js в проект

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import ExampleComponent from './components/ExampleComponent.vue';
+
+createApp({
+    components: { ExampleComponent }
+}).mount('#app');
