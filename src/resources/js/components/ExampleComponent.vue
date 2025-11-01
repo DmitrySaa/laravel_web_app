f23da690abe711a5cb31985b74e4e87f66d853dc
DmitrySaa <samss4598@gmail.com>
2025-11-01 16:37:36 +0400

    первый компонент Vue

---
+++ src/resources/js/components/ExampleComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>События:</h3>
+    <ul>
+      <li v-for="(event, index) in events" :key="index">
+        {{ event.title }} — {{ event.date }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "ExampleComponent",
+  props: {
+    events: Array
+  }
+}
+</script>
